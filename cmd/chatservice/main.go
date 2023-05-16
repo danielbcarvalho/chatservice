9d89ec39dbe2b042cfd2fee6e6017231998ff834
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2023-05-15 21:30:52 -0300

    feat: add grpc

--- cmd/chatservice/main.go
+++ cmd/chatservice/main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 
 	"github.com/danielbcarvalho/chatservice/configs"
-	// "github.com/danielbcarvalho/chatservice/internal/infra/grpc/server"
+	"github.com/danielbcarvalho/chatservice/internal/infra/grpc/server"
 	"github.com/danielbcarvalho/chatservice/internal/infra/repository"
 	"github.com/danielbcarvalho/chatservice/internal/infra/web"
 	"github.com/danielbcarvalho/chatservice/internal/infra/web/webserver"
 	"github.com/danielbcarvalho/chatservice/internal/usecase/chatcompletion"
+	"github.com/danielbcarvalho/chatservice/internal/usecase/chatcompletionstream"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/sashabaranov/go-openai"
 )
@@ -41,31 +42,31 @@ func main() {
 		InitialSystemMessage: configs.InitialChatMessage,
 	}
 
-	// chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
-	// 	Model:                configs.Model,
-	// 	ModelMaxTokens:       configs.ModelMaxTokens,
-	// 	Temperature:          float32(configs.Temperature),
-	// 	TopP:                 float32(configs.TopP),
-	// 	N:                    configs.N,
-	// 	Stop:                 configs.Stop,
-	// 	MaxTokens:            configs.MaxTokens,
-	// 	InitialSystemMessage: configs.InitialChatMessage,
-	// }
+	chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
+		Model:                configs.Model,
+		ModelMaxTokens:       configs.ModelMaxTokens,
+		Temperature:          float32(configs.Temperature),
+		TopP:                 float32(configs.TopP),
+		N:                    configs.N,
+		Stop:                 configs.Stop,
+		MaxTokens:            configs.MaxTokens,
+		InitialSystemMessage: configs.InitialChatMessage,
+	}
 
 	usecase := chatcompletion.NewChatCompletionUseCase(repo, client)
 
-	// streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
-	// usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
+	streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
+	usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
 
 	fmt.Println("Starting gRPC server on port " + configs.GRPCServerPort)
-	// grpcServer := server.NewGRPCServer(
-	// 	*usecaseStream,
-	// 	chatConfigStream,
-	// 	configs.GRPCServerPort,
-	// 	configs.AuthToken,
-	// 	streamChannel,
-	// )
-	// go grpcServer.Start()
+	grpcServer := server.NewGRPCServer(
+		*usecaseStream,
+		chatConfigStream,
+		configs.GRPCServerPort,
+		configs.AuthToken,
+		streamChannel,
+	)
+	go grpcServer.Start()
 
 	webserver := webserver.NewWebServer(":" + configs.WebServerPort)
 	webserverChatHandler := web.NewWebChatGPTHandler(*usecase, chatConfig, configs.AuthToken)
--- go.mod
+++ go.mod
@@ -9,16 +9,21 @@ require (
 )
 
 require (
+	github.com/devfullcycle/fclx/chatservice v0.0.0-20230422131742-87710452a3c6
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/pkoukk/tiktoken-go v0.1.1
 	github.com/sashabaranov/go-openai v1.5.8
 	github.com/spf13/viper v1.15.0
+	google.golang.org/grpc v1.52.0
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
 	github.com/dlclark/regexp2 v1.8.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/j178/tiktoken-go v0.2.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
@@ -27,8 +32,10 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20221227171554-f9683d7f8bef // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
